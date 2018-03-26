f429954a8f7106b8c8eec8ece9cab00c2cce440a
Jan Karger <punkerat76@gmail.com>
2018-03-26 21:06:03 +0200

    Add sample to show the paket.reference overriding bug

---
+++ src/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using MahApps.Metro.Controls;
+
+namespace PaketMultiTargetingTest
+{
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
